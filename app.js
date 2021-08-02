a7c2b3c887f7fde895e4802b023ebfe5094a8a72
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-02 15:32:56 -0500

    get artist id

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const artistInput = document.querySelector(".artist-input");
 
 artistForm.addEventListener("submit", e => {
   e.preventDefault();
-  performOperations(artistInput.value);
+  const token = getToken();
 })
 
 async function performOperations(query) {
   try {
     const token = await getToken();
+    console.log("token",token);
     const artistID = await getArtistID(token, query);
+
   } catch {
     console.error(error);
   }
@@ -31,7 +33,8 @@ async function getToken() {
     };
     
     $.ajax(settings).done(function (response) {
-      console.log(response);
+      console.log(response.access_token);
+      getArtistID(response.access_token)
       return response.access_token;
     });
   }
@@ -40,36 +43,22 @@ async function getToken() {
   }
 }
 
-getToken()
-
 async function getArtistID(token, query) {
   try {
-    query.replace(" ", "%20");
+    const query = artistInput.value.replace(" ", "%20");
+    console.log("artist", query);
     const url = `https://api.spotify.com/v1/search?q=${query}&type=artist`;
+    // const url = "https://api.spotify.com/v1/search?q=Katy%20Perry&type=artist";
     const response = await axios.get(url, {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     });
-    console.log(response.data);
-    return response.data;
+    console.log(response.data.artists.items[0].id);
+    return response.data.artists.items[0].id;
   } catch (error) {
     console.error(error);
   }
 }
 
 // const url = 'https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02/top-tracks?&market=US';
-
-
-// async function test(token) {
-//   try {
-//     const res = await axios.get(url, {
-//       headers: {
-//         'Authorization': `Bearer ${token}`
-//       }
-//     })
-//     console.log(res);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
