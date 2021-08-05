fad470839d4aa4e9ccd124a5a9ce1a76a1ada823
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-08-05 12:38:47 -0500

    move handling of to top button

--- app.js
+++ app.js
@@ -81,6 +81,10 @@ async function getTopSongs(artistID, moreOptionsAvailable) {
     if (moreOptionsAvailable ^ (showMore.classList.contains("show"))) {
       showMore.classList.toggle("show");
     }
+    //check for whether to top button should show
+    if ((tracks.length > 1) ^ (toTop.classList.contains("show"))) {
+      toTop.classList.toggle("show");
+    }
     //if artist has no top tracks
     if (tracks.length === 0) {
       const artist = await getArtistByID(artistID);
@@ -109,10 +113,7 @@ async function getTopSongs(artistID, moreOptionsAvailable) {
       trackDiv.append(nameEl, artistsEl, albumEl, imgEl);
       tracksDiv.append(trackDiv);
     })
-    //check for whether to top button should show
-    if ((tracksDiv.children.length > 1) ^ (toTop.classList.contains("show"))) {
-      toTop.classList.toggle("show");
-    }
+
     return response.data;
   } catch (error) {
     if (tokenExpired(error)) {
